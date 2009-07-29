a5bf3d3c146b119e5d042c0a53887782c267fb5f
Jakub Ł. Gustak <jlg@usrsrc.net>
2009-07-29 16:37:07 +0100

    DatasourceDir, LineFileSlicer implemented.
    Slicers now have base-class Slicer. Subclasses should implement:
    * __iter__() for key (value) generation,
    * _load() for value loading.
    
    When Slicer.send_as_input is True:
    * __iter__() generates value instead of key,
    * load() returns value immediately instead of real loading,
    * real loading move to _load() method.

--- pydra_server/cluster/tasks/datasource.py
+++ pydra_server/cluster/tasks/datasource.py
@@ -14,35 +14,107 @@ class DatasourceDict(object):
         self.store = dict
 
 
+    def __iter__(self):
+        """key generation"""
+        for key in self.store.iterkeys():
+            yield key, # tuple
+
     def load(self, key):
         """data reading"""
         # we are the source len(key) == 1: key[0] == key[-1]
         return self.store[key[-1]]
 
 
+class DatasourceDir(object):
+
+    def __init__(self, dir):
+        self.dir = dir
+
     def __iter__(self):
-        """key generation"""
-        for key in self.store.iterkeys():
-            yield key, # tuple
+        """generate key to files"""
+        files = os.walk(self.dir).__iter__().next()[2]
 
-
-class SequenceSlicer(object):
-
-    def __init__(self):
-        self.input = None
+        for filename in files:
+            yield filename,
 
 
     def load(self, key):
+        filename = key[-1]
+        path = os.path.join(self.dir, filename)
+        return open(path)
+
+
+class Slicer(object):
+
+    def __init__(self):
+        self.input = None
+        self.send_as_input = False
+
+
+    def __iter__(self):
+        # implement this
+        raise NotImplementedError
+
+
+    def load(self, key):
+        if self.send_as_input:
+            return key
+
+        return self._load(key)
+
+
+    def _load(self, key):
+        # implement this
+        raise NotImplementedError
+
+
+class SequenceSlicer(Slicer):
+
+    def __iter__(self):
+        """key generation"""
+        for input_key in self.input:
+            seq = self.input.load(input_key)
+            if self.send_as_input:
+                for input in seq:
+                    yield input
+            else:
+                for index in xrange(len(seq)):
+                    yield input_key + (index, )
+
+
+    def _load(self, key):
         """data reading"""
         parent, index = key[:-1], key[-1]
         sequence = self.input.load(parent)
         return sequence[index]
 
 
+class LineFileSlicer(Slicer):
+
     def __iter__(self):
-        """key generation"""
         for input_key in self.input:
-            seq_len = len(self.input.load(input_key))
-            for sequence_key in xrange(seq_len):
-                yield input_key + (sequence_key, )
+            with self.input.load(input_key) as f:
+                #must use readline:
+                # for line in f: consumes whole file and
+                # f.tell() become useless
+                offset = 0
+                line = f.readline()
+
+                while line:
+                    if self.send_as_input:
+                        yield line.strip()
+                    else:
+                        yield input_key + (offset, )
+
+                    offset = f.tell()
+                    line = f.readline()
+
+
+    def _load(self, key):
+        parent, offset = key[:-1], key[-1]
+        with self.input.load(parent) as f:
+            f.seek(offset)
+            line = f.readline().strip()
+
+        return line
 
--- pydra_server/cluster/tasks/tests/datasource.py
+++ pydra_server/cluster/tasks/tests/datasource.py
@@ -1,22 +1,27 @@
+from __future__ import with_statement
+
 import unittest
 
 from pydra_server.cluster.tasks.datasource import *
 import tempfile, shutil
 
+in_dict = { 
+            "k1": ['one', 'two', 'four', 'two', 'four', 'seven'],
+            "k2": ['seven', 'four', 'seven', 'seven'],
+            "k3": ['seven', 'four', 'seven', 'seven'],
+          }
 
-class Datasource_Test(unittest.TestCase):
+
+class DatasourceDict_Test(unittest.TestCase):
 
     def setUp(self):
-        self.in_dict = { 
-                    "k1": ['one', 'two', 'four', 'two', 'four', 'seven'],
-                    "k2": ['seven', 'four', 'seven', 'seven'],
-                    "k3": ['seven', 'four', 'seven', 'seven'],
-                       }
+        self.in_dict = in_dict
+        self.source = DatasourceDict(in_dict)
 
 
     def test_datasourcedict(self):
 
-        input = DatasourceDict(self.in_dict)
+        input = self.source
 
         for key in input:
             dkey = key[-1]
@@ -25,40 +30,101 @@ class Datasource_Test(unittest.TestCase):
 
     def test_sequenceslicer(self):
 
-        source = DatasourceDict(self.in_dict)
+        source = self.source
 
-        expected_seq = []
+        for send_as_input in (True, False):
 
-        for xs in self.in_dict.itervalues():
-            for x in xs:
-                expected_seq.append(x)
+            expected_seq = []
 
-        slicer = SequenceSlicer()
-        slicer.input = source
+            for xs in self.in_dict.itervalues():
+                for x in xs:
+                    expected_seq.append(x)
 
-        for key in slicer:
-            self.assertEqual(len(key), 2)
-            self.assertEqual(slicer.load(key), expected_seq.pop(0))
+            slicer = SequenceSlicer()
+            slicer.input = source
+            slicer.send_as_input = send_as_input
+
+            for key in slicer:
+                if not send_as_input:
+                    self.assertEqual(len(key), 2)
+                self.assertEqual(slicer.load(key), expected_seq.pop(0))
 
 
     def test_nested_sequenceslicer(self):
 
-        source = DatasourceDict(self.in_dict)
+        source = self.source
 
-        expected_seq = []
+        for send_as_input in (True, False):
 
-        for xs in self.in_dict.itervalues():
-            for str in xs:
-                for char in str:
-                    expected_seq.append(char)
+            expected_seq = []
 
-        outer_slicer = SequenceSlicer()
-        outer_slicer.input = source
+            for xs in self.in_dict.itervalues():
+                for str in xs:
+                    for char in str:
+                        expected_seq.append(char)
 
-        inner_slicer = SequenceSlicer()
-        inner_slicer.input = outer_slicer
+            outer_slicer = SequenceSlicer()
+            outer_slicer.input = source
 
-        for key in inner_slicer:
-            self.assertEqual(len(key), 3)
-            self.assertEqual(inner_slicer.load(key), expected_seq.pop(0))
+            inner_slicer = SequenceSlicer()
+            inner_slicer.input = outer_slicer
+            inner_slicer.send_as_input = send_as_input
+
+            for key in inner_slicer:
+                if not send_as_input:
+                    self.assertEqual(len(key), 3)
+                self.assertEqual(inner_slicer.load(key), expected_seq.pop(0))
+
+
+class DatasourceDir_Test(unittest.TestCase):
+
+    def setUp(self):
+        self.tempdir = tempfile.mkdtemp()
+        #tempfile.tempdir = self.tempdir
+
+        for filename, lines in in_dict.iteritems():
+            path = os.path.join(self.tempdir, filename)
+            with open(path, "w") as f:
+                for line in lines:
+                    f.write("%s\n" % line)
+
+        self.source = DatasourceDir(self.tempdir)
+
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
+
+
+    def test_datasourcedir(self):
+        input = self.source
+
+        for key in input:
+            dkey = key[-1]
+
+            with input.load(key) as f:
+                for line in f:
+                    line = line.strip()
+                    if line:
+                        self.assert_(line in in_dict[dkey])
+
+    def test_line_as_input_slicer(self):
+        source = self.source
+
+        for send_as_input in (True, False):
+
+            expected_seq = []
+
+            for xs in in_dict.itervalues():
+                for x in xs:
+                    expected_seq.append(x)
+
+            slicer = LineFileSlicer()
+            slicer.input = source
+            slicer.send_as_input = send_as_input
+
+            for key in slicer:
+                if not send_as_input:
+                    self.assertEqual(len(key), 2)
+                val = slicer.load(key)
+                self.assertEqual(val, expected_seq.pop(0), "failed on key %s" % str(key))
 
