ae19473b24eef3e95e8f4efa86aeeec516a59490
Peter Krenesky <peter@osuosl.org>
2009-08-15 22:35:39 -0700

    Distribution Prep - adding config file that points to installation specific directories

---
+++ pydra/config.py
@@ -0,0 +1,18 @@
+"""
+Pydra configuration - This file is used to specify paths used by pydra.  These
+are used to deal with platform specific oddities that occur when installing a
+package.  This file should be importable as pydra.config allowing these paths
+to be located.
+"""
+import sys
+
+CONFIG_DIR = '/etc/pydra'
+LOGGING_DIR = '/var/log/pydra'
+
+def load_settings():
+    """
+    Helper function for adding settings to the python path
+    """
+    sys.path.append(CONFIG_DIR)
+    import pydra_settings
+    return pydra_settings
