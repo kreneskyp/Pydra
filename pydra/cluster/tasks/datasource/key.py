b810fb504a5f3c040fd6e30b9a140e4a8e5fbc8a
Corbin Simpson <simpsoco@osuosl.org>
2010-03-03 08:30:29 -0800

    Actually serialize and restore class names.
    Tests still run fine.

--- pydra/cluster/tasks/datasource/key.py
+++ pydra/cluster/tasks/datasource/key.py
@@ -4,49 +4,6 @@ This probably shouldn't be specific to the datasource API.
 
 import functools
 
-def keyinit(f):
-    """
-    Simple decorator to be placed on a class's __init__. Instances of the
-    class will have a _key attribute, which can be used to serialize and
-    deseralize the class.
-
-    Keyable classes can be nested inside other keyable classes and will still
-    serialize properly.
-    """
-
-    @functools.wraps(f)
-    def init(self, *args, **kwargs):
-        f(self, *args, **kwargs)
-
-        kids = []
-        while args and hasattr(args[0], "key"):
-            kids.append(args[0].key)
-            args = args[1:]
-           
-        self.key = (self.__class__, kids, args, kwargs)
-
-    return init
-
-def keyable(c):
-    """
-    Simple class decorator to make a class keyable.
-    """
-
-    c.__init__ = keyinit(c.__init__)
-    return c
-
-def instance_from_key(key):
-    """
-    Instantiate an object from a key.
-    """
-
-    cls, kids, args, kwargs = key
-
-    if kids:
-        args = [instance_from_key(i) for i in kids] + list(args)
-
-    return cls(*args, **kwargs)
-
 def save_class(cls):
     """
     Serialize a class handle.
@@ -77,3 +34,48 @@ def restore_class(cls, g={}, l={}):
             return getattr(m, name)
     except ImportError:
         pass
+
+def keyinit(f):
+    """
+    Simple decorator to be placed on a class's __init__. Instances of the
+    class will have a _key attribute, which can be used to serialize and
+    deseralize the class.
+
+    Keyable classes can be nested inside other keyable classes and will still
+    serialize properly.
+    """
+
+    @functools.wraps(f)
+    def init(self, *args, **kwargs):
+        f(self, *args, **kwargs)
+
+        kids = []
+        while args and hasattr(args[0], "key"):
+            kids.append(args[0].key)
+            args = args[1:]
+           
+        self.key = (save_class(self.__class__), kids, args, kwargs)
+
+    return init
+
+def keyable(c):
+    """
+    Simple class decorator to make a class keyable.
+    """
+
+    c.__init__ = keyinit(c.__init__)
+    return c
+
+def instance_from_key(key):
+    """
+    Instantiate an object from a key.
+    """
+
+    cls, kids, args, kwargs = key
+
+    cls = restore_class(cls)
+
+    if kids:
+        args = [instance_from_key(i) for i in kids] + list(args)
+
+    return cls(*args, **kwargs)
