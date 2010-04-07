dfa2877f08d3f6eb47eb6e97fe9d420d76ba9a42
Corbin Simpson <simpsoco@osuosl.org>
2010-04-07 14:13:43 -0700

    Add SQLite backend. Totally untested.

---
+++ pydra/cluster/tasks/datasource/backend.py
@@ -0,0 +1,38 @@
+try:
+    import pysqlite2.dbapi2
+except ImportError:
+    pysqlite2 = None
+    print "Warning: pysqlite2 couldn't be found, SQLiteBackend will not work."
+
+from pydra.cluster.tasks.datasource.key import keyable
+
+@keyable
+class SQLiteBackend(object):
+    """
+    Backend for interfacing with SQLite databases.
+    """
+
+    def __init__(self):
+        if not pysqlite2:
+            raise Exception, "SQLite support could not be found."
+
+        self.handle = None
+
+    def connect(self, db):
+        """
+        Open a database.
+
+        Relative paths may not be handled correctly.
+
+        The special name ":memory:" will open a temporary memory-backed
+        database.
+        """
+
+        self.handle = pysqlite2.dbapi2.Connection(db)
+
+    def disconnect(self):
+        self.handle.close()
+
+    @property
+    def connected(self):
+        return bool(self.handle)
