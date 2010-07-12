fe294a92d18ade6b209e6652bfa0aeeaca7b88c2
Corbin Simpson <simpsoco@osuosl.org>
2010-07-12 14:14:44 -0700

    datasource: Add test for unpack and validate.
    Kind of useless at the moment, sorry.

---
+++ pydra/cluster/tasks/datasource/tests/init.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import unittest
+
+from pydra.cluster.tasks.datasource import unpack, validate
+
+class ValidateTest(unittest.TestCase):
+
+    pass
+
+class UnpackTest(unittest.TestCase):
+
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
